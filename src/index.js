a5f49f4fe607dc64b573c0e948f978ebdf56f496
chris <me@chriz.dev>
2017-02-21 16:04:27 +0800

    add src

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function featureTest( property, value, { context = document } = {}) {
+  let el = context.createElement('test')
+  const cssText = ['-webkit-', '-moz-', '-ms-', '-o-', ''].map(
+  (x) => `${property}:${x}${value};`
+  )
+
+   el.style.cssText = cssText.join('')
+
+   return el.style.cssText
+
+}
+
+module.exports = featureTest
+
