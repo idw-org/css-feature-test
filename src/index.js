4db4a6a4ec6d5dd9afcc7bd8f054cd28f3318755
chris <me@chriz.dev>
2017-02-21 19:30:22 +0800

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-function featureTest( property, value, { context = document } = {}) {
-  let el = context.createElement('test')
-  const cssText = ['-webkit-', '-moz-', '-ms-', '-o-', ''].map(
-  (x) => `${property}:${x}${value};`
-  )
+function featureTest(property, value, { context = document } = {}) {
+  let el = context.createElement("test");
+  const cssText = ["-webkit-", "-moz-", "-ms-", "-o-", ""].map(
+    x => `${property}:${x}${value};`
+  );
 
-   el.style.cssText = cssText.join('')
-
-   return el.style.cssText
-
+  el.style.cssText = cssText.join("");
+  return el.style.cssText;
 }
 
-module.exports = featureTest
-
+module.exports = featureTest;
